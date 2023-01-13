d03e200bafe1367b0e8f98b31bba0d66e2c6cac0
Anderson Lima <anderson.lima-ext@olxbr.com>
2023-01-13 18:18:49 -0300

    chore: add new test config, pre push and commit

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   roots: ["<rootDir>/src"],
   collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}"],
   coverageDirectory: "coverage",
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
   testEnvironment: "node",
   transform: {
     ".+\\.(ts|tsx)$": "ts-jest",
