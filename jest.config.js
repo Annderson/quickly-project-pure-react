d2ebf77c3006bb2fb277c9bcc05e078552bfb6f5
Anderson Lima <anderson.lima-ext@olxbr.com>
2023-01-17 08:44:10 -0300

    chore: install new libs and webpack config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}", "!**/*.d.ts"],
   coverageDirectory: "coverage",
   moduleNameMapper: {
     "@/(.*)": "<rootDir>/src/$1",
   },
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   transform: {
     ".+\\.(ts|tsx)$": "ts-jest",
+    "\\.scss$": "identity-obj-proxy",
   },
 };
